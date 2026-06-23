30c56eb87b51de4cff92c3c96593251b1e810861
Macarena <macarena.p.r@outlook.es>
2026-06-23 13:43:10 -0400

    arreglo de img heros theme-9

--- Frontend/restaurante-landings/src/main.jsx
+++ Frontend/restaurante-landings/src/main.jsx
@@ -6,6 +6,7 @@ import './themes/themes.css'
 import './styles/theme-layout.css'
 import './themes/theme-1.css'
 import './themes/theme_8.css'
+import './themes/theme_9.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById("root")).render(
